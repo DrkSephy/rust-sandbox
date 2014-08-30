124a74dc96024e1d757343c92029069b071ef2d3
David Leonard <sephirothcloud1025@yahoo.com>
2014-08-30 18:13:36 -0400

    Adding literals and operators example

---
+++ src/literals_operators.rs
@@ -0,0 +1,9 @@
+fn main(){
+    /*
+     * Integers can be expressed using hexadecimal, octal or binary notation.
+     * Hexadecimal: 0x, Octal: 0o, Binary: 0b
+     * `u` suffix to indicate literal is an unsigned int
+     * `i` suffix to indicate literal is a signed int
+    */
+    println!("1 + 2 = {}", 1u + 2);
+}
