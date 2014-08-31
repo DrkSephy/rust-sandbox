a8f8a7c75a5a00bf0802b36cc4d340f4579e244c
David Leonard <sephirothcloud1025@yahoo.com>
2014-08-31 08:26:10 -0400

    Adding example of including external libraries

---
+++ src/extern.rs
@@ -0,0 +1,13 @@
+// executable.rs
+// Link to `liberty`, import items under the `erty` module
+extern crate erty;
+
+fn main() {
+    erty::public_function();
+
+    // Error! `private_function` is private
+    // erty::private_function();
+
+    erty::indirect_access();
+}
+
